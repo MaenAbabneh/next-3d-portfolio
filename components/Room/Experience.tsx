0ab56925370eb1f7cae8c6753fbfc44366e60402
MaenAbabneh <maenababneh@gmail.com>
2026-03-05 22:04:41 +0300

    feat: add ThemeToggle component to Experience for dynamic theme switching

--- components/Room/Experience.tsx
+++ components/Room/Experience.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import { Model } from "./Room"; // تأكد من المسار
 
 import { Loader } from "../LoadingScreen";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function Experience() {
   const [start, setStart] = useState(false);
@@ -14,7 +15,9 @@ export default function Experience() {
   return (
     <div className="w-full h-screen bg-[#111111]">
       <Loader started={start} onStarted={() => setStart(true)} />
-
+      <div className="absolute top-10 right-14 z-10">
+        <ThemeToggle />
+      </div>
       <Canvas camera={{ position: [5, 3, 5], fov: 45 }}>
         <Environment preset="city" />
         <OrbitControls makeDefault />
