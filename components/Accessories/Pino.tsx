8d0488fb1bcf2f3aa3e86fbffa97756ef9a003ed
MaenAbabneh <maenababneh@gmail.com>
2026-03-05 22:04:46 +0300

    refactor: enhance type definitions and improve node validation in Pino component

--- components/Accessories/Pino.tsx
+++ components/Accessories/Pino.tsx
@@ -2,15 +2,20 @@
 
 import { PianoKey } from "../PianoKey";
 import { KEYS, SUFFIX } from "@/constant/utils";
+import * as THREE from "three";
+import type { GLTFResult } from "@/types/room.types";
 
-export function Pino({ nodes }: { nodes: any }) {
+interface PinoProps {
+  nodes: GLTFResult["nodes"];
+}
+export function Pino({ nodes }: PinoProps) {
   return (
     <group>
       {KEYS.map((keyName, index) => {
         const fullName = `${keyName}${SUFFIX}`;
-        const node = nodes[fullName];
+        const node = nodes[fullName as keyof typeof nodes];
 
-        if (!node) return null;
+        if (!node || !(node instanceof THREE.Mesh)) return null;
 
         return (
           <PianoKey key={fullName} node={node} index={index} name={fullName} />
