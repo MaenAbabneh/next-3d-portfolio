28f665d58fc12e504006c623548e1bf6f3a6cc0a
MaenAbabneh <maenababneh@gmail.com>
2026-03-05 22:02:36 +0300

    refactor: remove ThemeToggle component from Home for cleaner layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { ThemeToggle } from "@/components/ThemeToggle";
 import Experience from "@/components/Room/Experience";
 
 export default function Home() {
   return (
     <main>
-      <div className="absolute top-4 right-4 z-10">
-        <ThemeToggle />
-      </div>
       <Experience />
     </main>
   );
