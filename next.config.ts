5b15c06cf4b756017fbdcdaae42386c302701318
MaenAbabneh <maenababneh@gmail.com>
2026-03-06 15:47:19 +0300

    feat: add experimental configuration options for turbopack and package imports

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  cacheComponents: true,
+
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    optimizePackageImports: [
+      "@react-three/drei",
+      "@gsap/react",
+      "howler",
+      "react-icons",
+      "three",
+    ],
+  },
 };
 
 export default nextConfig;
