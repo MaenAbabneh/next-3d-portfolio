2b5072bccd97fbabee82fb99f7cf75edc2894c79
MaenAbabneh <maenababneh@gmail.com>
2026-03-08 22:27:23 +0300

    feat: add Overlay and ImageViewer components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import ImageViewer from "@/components/Overlay/ImageViewer";
+import Overlay from "@/components/Overlay/Overlay";
 import Experience from "@/components/Room/Experience";
 
 export default function Home() {
   return (
     <main>
       <Experience />
+      <Overlay />
+      <ImageViewer />
     </main>
   );
 }
