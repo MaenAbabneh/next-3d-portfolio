10e7ff870d01af4cc6935da94b7b1b7f67771446
MaenAbabneh <maenababneh@outlook.com>
2026-03-09 12:38:01 +0300

    feat: enhance camera controls in Experience component with improved OrbitControls settings

--- components/Room/Experience.tsx
+++ components/Room/Experience.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Suspense } from "react";
-import { Model } from "./Room"; // تأكد من المسار
+import { Model } from "./Room";
 
 import { LoadingScreen } from "../LoadingScreen";
 import { ThemeToggle } from "@/components/Toggle/ThemeToggle";
@@ -28,9 +28,20 @@ export default function Experience() {
         <ThemeToggle />
       </div>
 
-      <Canvas camera={{ position: [5, 3, 5], fov: 45 }}>
+      <Canvas camera={{ position: [5, 4, 5], fov: 45 }}>
         <Environment preset="city" />
-        <OrbitControls makeDefault />
+        <OrbitControls
+          makeDefault
+          enableDamping={true}
+          minDistance={4}
+          maxDistance={13}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2 - 0.1}
+          minAzimuthAngle={-Math.PI / 38}
+          maxAzimuthAngle={Math.PI / 2}
+          enablePan={false}
+          target={[0, 1, 0]}
+        />
         <Suspense fallback={null}>
           <Model started={startExperience} />
         </Suspense>
