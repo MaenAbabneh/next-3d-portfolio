532356d7016dbe9f355b3ea27feadec723e4dcf4
MaenAbabneh <maenababneh@outlook.com>
2026-03-14 21:43:59 +0300

    fix: clean up comments and improve button layout in ModalLayout component

--- components/overlay/ModalLayout.tsx
+++ components/overlay/ModalLayout.tsx
@@ -9,7 +9,7 @@ import { IoClose } from "react-icons/io5";
 interface ModalLayoutProps {
   children: React.ReactNode;
   title: string;
-  className?: string; // لتخصيص العرض والارتفاع حسب القسم
+  className?: string;
 }
 
 export default function ModalLayout({
@@ -69,7 +69,6 @@ export default function ModalLayout({
 
       gsap.killTweensOf(button);
 
-      // أنيميشن الزر عند الضغط
       gsap.to(button, {
         scale: 1.5,
         duration: 0.2,
@@ -80,7 +79,7 @@ export default function ModalLayout({
             duration: 0.2,
             onComplete: () => {
               gsap.set(button, { clearProps: "transform" });
-              closeOverlay(); // إغلاق النافذة
+              closeOverlay();
             },
           });
         },
@@ -91,28 +90,28 @@ export default function ModalLayout({
 
   return (
     <div
-      className={`relative flex flex-col items-center bg-base-cream dark:bg-base-blue-light  border-8 border-base-blue dark:border-base-blue-dark rounded-[25px] p-6 md:p-8 shadow-2xl ${className}`}
+      className={`relative flex flex-col items-center bg-base-cream dark:bg-base-blue-light border-8 border-base-blue dark:border-base-blue-dark rounded-[25px] p-6 md:p-8 shadow-2xl max-h-full ${className}`}
     >
-      {/* 1. العنوان (Modal Title) */}
-      <h1 className="relative -top-2 px-10 py-2 rounded-full border-4 border-base-blue dark:border-base-blue-dark text-2xl md:text-3xl font-bold text-base-brwan shadow-md mb-6 text-center font-serif ">
+      {/* 1. العنوان */}
+      <h1 className="relative -top-2 px-10 py-2 rounded-full border-4 border-base-blue dark:border-base-blue-dark text-xl md:text-3xl font-bold text-base-brwan shadow-md mb-6 text-center font-serif shrink-0">
         {title}
       </h1>
 
-      {/* 2. المحتوى (Scrollable Area) */}
+      {/* 2. المحتوى */}
       <div className="w-full h-full flex-1 overflow-y-auto custom-scrollbar px-2">
         {children}
       </div>
-
-      {/* 3. زر الإغلاق (Exit Button) - نفس الـ SVG */}
-      <button
-        ref={buttonRef}
-        onClick={handleClose}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className="absolute -top-10 -left-6 md:-left-10 flex items-center justify-center w-16 h-16 md:w-20 md:h-20 bg-base-cream dark:bg-base-blue-light border-[6px] border-base-blue dark:border-base-blue-dark rounded-2xl shadow-lg origin-center z-50"
-      >
-        <IoClose className="w-8 h-8 md:w-15 md:h-15 text-base-blue dark:text-base-blue-dark" />
-      </button>
+      <div className="absolute z-50 -bottom-10 left-1/2 -translate-x-1/2 md:bottom-auto md:-top-10 md:-left-10 md:translate-x-0">
+        <button
+          ref={buttonRef}
+          onClick={handleClose}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 bg-base-cream dark:bg-base-blue-light border-[6px] border-base-blue dark:border-base-blue-dark rounded-2xl shadow-lg"
+        >
+          <IoClose className="w-8 h-8 md:w-12 md:h-12 text-base-blue dark:text-base-blue-dark" />
+        </button>
+      </div>
     </div>
   );
 }
